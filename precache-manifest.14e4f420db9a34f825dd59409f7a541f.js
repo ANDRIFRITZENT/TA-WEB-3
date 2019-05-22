6cd6cbc56badf4af357f867955d211cdf550fbb5
ANDRIFRITZENT <andryfritzent05@gmail.com>
2019-05-22 20:13:21 +0700

    Updates

---
+++ precache-manifest.14e4f420db9a34f825dd59409f7a541f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45af6f11587e4223b2e47c5929ddd9ee",
+    "url": "/TA-WEB-3/index.html"
+  },
+  {
+    "revision": "eabada6e0e9d0ecacbe5",
+    "url": "/TA-WEB-3/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/TA-WEB-3/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "eabada6e0e9d0ecacbe5",
+    "url": "/TA-WEB-3/static/js/main.bfd08057.chunk.js"
+  },
+  {
+    "revision": "612d5fe054c417b5ed0a",
+    "url": "/TA-WEB-3/static/js/runtime~main.282a4eec.js"
+  }
+]);
